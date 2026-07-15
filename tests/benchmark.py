d24d596226c62c43a457f7c6889032f9ace890c2
Samuel Castro <casariasc@gmail.com>
2026-07-15 00:58:17 -0500

    Benchmark AG y LS

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -8,6 +8,8 @@ except ImportError:
     from src.bruteforce import resolver_fuerza_bruta
 
 from src.backtracking import resolver_backtracking
+from src.local_search import resolver_local_search
+from src.genetic_algorithm import solve_completion_ga
 from tests.generator import generar_tablero_aleatorio
 
 def ejecutar_pruebas_rendimiento():
@@ -22,7 +24,9 @@ def ejecutar_pruebas_rendimiento():
     datos_grafica = {
         "tamanos_n": tamanos,
         "tiempos_brute_force": [],
-        "tiempos_backtracking": []
+        "tiempos_backtracking": [],
+        "tiempos_local_search": [],
+        "tiempos_genetic_algorithm": []
     }
 
     print("Iniciando pruebas de rendimiento...")
@@ -34,24 +38,41 @@ def ejecutar_pruebas_rendimiento():
         # 1 generamos el tablero sorpresa
         tablero_inicial = generar_tablero_aleatorio(n, reinas_iniciales_por_tablero)
         
-        # 2 medimos el tiempo de fuerza bruta
+        #  medimos el tiempo de fuerza bruta
         inicio_fb = time.perf_counter()
         resolver_fuerza_bruta(n, tablero_inicial)
         fin_fb = time.perf_counter()
         tiempo_fb = (fin_fb - inicio_fb) * 1000  
         
-        # 3 medimos el tiempo de backtracking
+        #  medimos el tiempo de backtracking
         inicio_bt = time.perf_counter()
         resolver_backtracking(n, tablero_inicial)
         fin_bt = time.perf_counter()
         tiempo_bt = (fin_bt - inicio_bt) * 1000  
 
+        #  medimos el tiempo de local_search
+        inicio_ls = time.perf_counter()
+        resolver_local_search(n, tablero_inicial)
+        fin_ls = time.perf_counter()
+        tiempo_ls = (fin_ls - inicio_ls) * 1000  
+
+        #  medimos el tiempo de genetic_algorithm
+        inicio_ga = time.perf_counter()
+        solve_completion_ga(n, tablero_inicial)
+        fin_ga = time.perf_counter()
+        tiempo_ga = (fin_ga - inicio_ga) * 1000  
+
         # 4 guardamos los resultados
         datos_grafica["tiempos_brute_force"].append(tiempo_fb)
         datos_grafica["tiempos_backtracking"].append(tiempo_bt)
+        datos_grafica["tiempos_local_search"].append(tiempo_ls)
+        datos_grafica["tiempos_genetic_algorithm"].append(tiempo_ga)
         
         print(f"  - Fuerza Bruta: {tiempo_fb:.4f} ms")
         print(f"  - Backtracking: {tiempo_bt:.4f} ms")
+        print(f"  - Local Search: {tiempo_fb:.4f} ms")
+        print(f"  - Algoritmo Genético: {tiempo_bt:.4f} ms")
+
 
     # 5 exportamos a un archivo JSON
     ruta_json = os.path.join(os.path.dirname(__file__), 'resultados_rendimiento.json')
