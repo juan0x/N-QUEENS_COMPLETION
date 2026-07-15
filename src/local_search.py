bdd24a4414409fd4332aa9e4d1ea6237c45061de
Juan Garcia <jandresgar@hotmail.com>
2026-07-15 01:42:03 -0500

    Max_Iter agregado en local_search

--- src/local_search.py
+++ src/local_search.py
@@ -22,59 +22,56 @@ IDEA 3: ALGORITMO QUE REUBIQUE ÚNICAMENTE LAS REINAS NO INICIALES
 '''
 from numpy import random
 
-# Función para ver si hay amenazas entre reinas
 def conflictos(reinas):
-  for (r1,c1) in reinas:
-    for (r2,c2) in reinas:
-      if (r1,c1) != (r2,c2):
-        if r1 == r2 or c1 == c2 or r1-c1 == r2-c2 or r1+c1 == r2+c2:
-          return True
-  return False
+    for (r1, c1) in reinas:
+        for (r2, c2) in reinas:
+            if (r1, c1) != (r2, c2):
+                if r1 == r2 or c1 == c2 or r1-c1 == r2-c2 or r1+c1 == r2+c2:
+                    return True
+    return False
 
-# Función para ver la reina con más amenazas
 def reina_max_conflictos(reinas, reinas_iniciales):
-  reina_max_conflictos = None
-  n_max_conflictos = -1
+    reina_max = None
+    n_max_conflictos = -1
 
-  for (r1,c1) in reinas:
-      if not (r1,c1) in reinas_iniciales:
-        n_conflictos = 0
-        for (r2,c2) in reinas:
-            if (r1,c1) != (r2,c2):
-              if r1 == r2 or c1 == c2 or r1-c1 == r2-c2 or r1+c1 == r2+c2:
-                n_conflictos+=1
+    for (r1, c1) in reinas:
+        if not (r1, c1) in reinas_iniciales:
+            n_conflictos = 0
+            for (r2, c2) in reinas:
+                if (r1, c1) != (r2, c2):
+                    if r1 == r2 or c1 == c2 or r1-c1 == r2-c2 or r1+c1 == r2+c2:
+                        n_conflictos += 1
 
-        if n_conflictos > n_max_conflictos:
-            reina_max_conflictos = (r1,c1)
-            n_max_conflictos = n_conflictos
-  return reina_max_conflictos
+            if n_conflictos > n_max_conflictos:
+                reina_max = (r1, c1)
+                n_max_conflictos = n_conflictos
+    return reina_max
 
-# FUNCIÓN PRINCIPAL
-def resolver_local_search(n, reinas_iniciales):
-
-    #Verificar si una configuración es solucionable
+def resolver_local_search(n, reinas_iniciales, max_iter=1000):
     if conflictos(reinas_iniciales):
-        return False
+        return None
 
-    #Crear el primer tablero aleatorio
     reinas_colocadas = set(reinas_iniciales)
-    filas_ocupadas = {r for (r,c) in reinas_iniciales}
-    for i in range(n):
-        if i in filas_ocupadas:
-            pass
-        else:
-            col_aleatoria = random.randint(n)
-            reinas_colocadas.add((i,col_aleatoria))
+    filas_ocupadas = {r for (r, c) in reinas_iniciales}
     
-    #Mover la reina con más amenazas hasta encontrar la solución
-    while conflictos(reinas_colocadas):
-        reina_conflictiva = reina_max_conflictos(reinas_colocadas,reinas_iniciales)
+    for i in range(n):
+        if i not in filas_ocupadas:
+            col_aleatoria = random.randint(0, n-1)
+            reinas_colocadas.add((i, col_aleatoria))
+    
+    iteraciones = 0
+    while conflictos(reinas_colocadas) and iteraciones < max_iter:
+        reina_conflictiva = reina_max_conflictos(reinas_colocadas, reinas_iniciales)
         if reina_conflictiva is None:
             break
-        col_aleatoria = random.randint(n)
+            
+        col_aleatoria = random.randint(0, n-1)
         reinas_colocadas.remove(reina_conflictiva)
-        reinas_colocadas.add((reina_conflictiva[0],col_aleatoria))
+        reinas_colocadas.add((reina_conflictiva[0], col_aleatoria))
+        iteraciones += 1
 
+    # Si salió del while pero aún hay conflictos, significa que se rindió por max_iter
+    if conflictos(reinas_colocadas):
+        return None
+        
     return list(reinas_colocadas)
-
-print(resolver_local_search(4,[]))
